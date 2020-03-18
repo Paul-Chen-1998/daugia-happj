f6c0efcef77e51df52a37617d6327b2f22b50231
Vinh <beminhquan3@gmail.com>
2020-03-18 12:32:44 +0700

    hoan thanh ho so

--- lib/pages/HoSo.dart
+++ lib/pages/HoSo.dart
@@ -140,179 +140,181 @@ class _BodyHoSoState extends State<BodyHoSo> {
         ],
       ),
     );
-    return new Container(
+    Widget _hoSoCaNhan = new Container(
+      padding: EdgeInsets.all(0.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.white,
+      child: new ListView(
         padding: EdgeInsets.all(0.0),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: new ListView(
-          padding: EdgeInsets.all(0.0),
-          children: <Widget>[
-            //background
-            new Container(
-              height: 300,
-              child: new Image.asset(
-                'images/hoso/backgroundhoso.jpg',
-                fit: BoxFit.cover,
-              ),
+        children: <Widget>[
+          //background
+          new Container(
+            height: 300,
+            child: new Image.asset(
+              'images/hoso/backgroundhoso.jpg',
+              fit: BoxFit.cover,
             ),
-            //avatar, name
-            new Container(
-              height: 80,
-              color: Colors.white,
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                          new BorderRadius.all(new Radius.circular(50.0)),
-                      border: new Border.all(
-                        color: Colors.black,
-                        width: 1.0,
-                      ),
-                    ),
-                    margin: EdgeInsets.only(left: 8, top: 5),
-                    width: 80,
-                    height: 80,
-                    child: new CircleAvatar(
-                      radius: 2,
-                      backgroundImage: AssetImage('images/hoso/user.jpg'),
+          ),
+          //avatar, name
+          new Container(
+            height: 80,
+            color: Colors.white,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                    new BorderRadius.all(new Radius.circular(50.0)),
+                    border: new Border.all(
+                      color: Colors.black,
+                      width: 1.0,
                     ),
                   ),
-                  Expanded(
-                    child: new ListTile(
-                      onTap: () {},
-                      title: new Text(
-                        'Thế Vinh',
+                  margin: EdgeInsets.only(left: 8, top: 5),
+                  width: 80,
+                  height: 80,
+                  child: new CircleAvatar(
+                    radius: 2,
+                    backgroundImage: AssetImage('images/hoso/user.jpg'),
+                  ),
+                ),
+                Expanded(
+                  child: new ListTile(
+                    onTap: () {},
+                    title: new Text(
+                      'Thế Vinh',
+                      style: new TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.w700),
+                    ),
+                    subtitle: new Text('the.vinh12121',
                         style: new TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.w700),
-                      ),
-                      subtitle: new Text('the.vinh12121',
-                          style: new TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.w700)),
+                            fontSize: 15, fontWeight: FontWeight.w700)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          new Divider(
+            indent: 20,
+            endIndent: 20,
+            color: Colors.black,
+            thickness: 1,
+          ),
+          //choose
+          new Container(
+            child: new Column(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    new CustomListTile(
+                        'Tài khoản', 'images/hoso/user.png', 35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
                     ),
-                  )
-                ],
-              ),
-            ),
-            new Divider(
-              indent: 20,
-              endIndent: 20,
-              color: Colors.black,
-              thickness: 1,
-            ),
-            //choose
-            new Container(
-              child: new Column(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Tài khoản', 'images/hoso/user.png',
-                          35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Địa chỉ giao hàng',
-                          'images/hoso/delivery.png', 35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Hỗ trợ khách hàng',
-                          'images/hoso/question.png', 35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  //images/hoso/quyenungdung.png
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Quyền khách hàng',
-                          'images/hoso/quyenungdung.png', 35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Trợ giúp', 'images/hoso/trogiup.png',
-                          35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  //
-                  Column(
-                    children: <Widget>[
-                      new CustomListTile('Về ứng dụng', 'images/hoso/law.png',
-                          35.0, 35.0, () {}),
-                      new Divider(
-                        indent: 0,
-                        endIndent: 0,
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      new Container(
-                        padding: EdgeInsets.all(0.0),
-                        child: new Material(
-                          child: new InkWell(
-                            splashColor: Colors.green,
-                            onTap: () {},
-                            child: ListTile(
-                              title: Text(
-                                'Đăng xuất',
-                                style: new TextStyle(
-                                    fontSize: 20.0,
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              leading: new Image.asset('images/hoso/lock.png',
-                                  fit: BoxFit.cover, width: 35, height: 35),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    new CustomListTile('Địa chỉ giao hàng',
+                        'images/hoso/delivery.png', 35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    new CustomListTile('Hỗ trợ khách hàng',
+                        'images/hoso/question.png', 35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                //images/hoso/quyenungdung.png
+                Column(
+                  children: <Widget>[
+                    new CustomListTile('Quyền khách hàng',
+                        'images/hoso/quyenungdung.png', 35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    new CustomListTile('Trợ giúp', 'images/hoso/trogiup.png',
+                        35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                //
+                Column(
+                  children: <Widget>[
+                    new CustomListTile('Về ứng dụng', 'images/hoso/law.png',
+                        35.0, 35.0, () {}),
+                    new Divider(
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    new Container(
+                      padding: EdgeInsets.all(0.0),
+                      child: new Material(
+                        child: new InkWell(
+                          splashColor: Colors.green,
+                          onTap: () {},
+                          child: ListTile(
+                            title: Text(
+                              'Đăng xuất',
+                              style: new TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600),
                             ),
+                            leading: new Image.asset('images/hoso/lock.png',
+                                fit: BoxFit.cover, width: 35, height: 35),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 90,
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 90,
+                )
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
+    return _hoSoCaNhan;
   }
 
   void _onClickLogIn() {
